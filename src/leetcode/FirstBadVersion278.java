fd0b29ba2c2de683a4b9f9b1bac0344cd7d3ef66
codemachine <itsrohan999@gmail.com>
2023-03-08 21:43:24 +0530

    Added leetcode 278

---
+++ src/leetcode/FirstBadVersion278.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class FirstBadVersion278 {
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while(l < r) {
+            int mid = l + (r - l)/2;
+            if(isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
