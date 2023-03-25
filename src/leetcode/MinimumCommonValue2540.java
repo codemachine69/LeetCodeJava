965a09da15b98723e14f0a27ec86a48c87a7f413
codemachine <itsrohan999@gmail.com>
2023-03-25 16:56:18 +0530

    Added leetcode 2540

---
+++ src/leetcode/MinimumCommonValue2540.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class MinimumCommonValue2540 {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int p1 = 0, p2 = 0, n1 = nums1.length, n2 = nums2.length;
+        while(p1 != n1 && p2 != n2) {
+            if(nums1[p1] < nums2[p2]) p1++;
+            else if(nums1[p1] > nums2[p2]) p2++;
+            else return nums1[p1];
+        }
+        return -1;
+    }
+}
