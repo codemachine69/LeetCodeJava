bb2141c9de16561939cbc9cbdb04df2cdb8a2b20
codemachine <itsrohan999@gmail.com>
2023-05-22 22:47:16 +0530

    Added leetcode 1758

---
+++ src/leetcode/MinAlternateBinStr1758.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class MinAlternateBinStr1758 {
+    public int minOperations(String s) {
+        int res = 0, n = s.length();
+        for (int i = 0; i < n; i++)
+            if (s.charAt(i) - '0' == i % 2) res++;
+        return Math.min(res, n - res);
+    }
+}
