c7d59b8561ac535b7136177f66c30378718a931d
codemachine <itsrohan999@gmail.com>
2023-06-03 00:43:40 +0530

    Removed ListNode class

--- src/leetcode/AddTwoNumbers2.java
+++ src/leetcode/AddTwoNumbers2.java
@@ -12,24 +12,7 @@ public class AddTwoNumbers2 {
      * }
      */
 
-    class ListNode {
-        int val;
-        ListNode next;
 
-        ListNode() {
-        }
-
-        ;
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
 
     class Solution {
