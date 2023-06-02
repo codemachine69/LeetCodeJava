3c76dde0e01dd6088bcb4c5461c2d4ee04b00328
codemachine <itsrohan999@gmail.com>
2023-06-03 00:44:13 +0530

    Added Leetcode 234

---
+++ src/leetcode/PalindromeLinkedList234.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class PalindromeLinkedList234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        ListNode left = head, right = prev;
+        while (right != null) {
+            if (left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+}
