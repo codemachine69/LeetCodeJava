f7c630052bf63293cf324fe785247a9accb5ab7a
codemachine <itsrohan999@gmail.com>
2023-06-22 17:14:58 +0530

    Added LeetCode 236

---
+++ src/leetcode/LowestCommonAncestorBT236.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class LowestCommonAncestorBT236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left == null && right == null) return null;
+        return left == null ? right : left;
+    }
+}
