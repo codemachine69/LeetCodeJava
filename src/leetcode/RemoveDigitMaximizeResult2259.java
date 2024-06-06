03efa88d1def9bbee73d650b42fa5e5ae2e5843b
rohan <itsrohan999@gmail.com>
2024-06-06 13:34:11 -0400

    Added LeetCode 2259

---
+++ src/leetcode/RemoveDigitMaximizeResult2259.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class RemoveDigitMaximizeResult2259 {
+    public String removeDigit(String number, char digit) {
+
+        for (int i = 0; i < number.length() - 1; i++) {
+            if (number.charAt(i) == digit && number.charAt(i + 1) > digit) {
+                return number.substring(0, i) + number.substring(i + 1);
+            }
+        }
+
+        for (int i = number.length() - 1; i > 0; i--) {
+            if (number.charAt(i) == digit) {
+                return number.substring(0, i) + number.substring(i + 1);
+            }
+        }
+
+        return "";
+    }
+}
