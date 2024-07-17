1f7545a3af960403ce1a3b5252a39dc35de55438
rohan <itsrohan999@gmail.com>
2024-07-16 20:38:28 -0400

    Added LeetCode 210 improved

--- src/leetcode/CourseScheduleII210.java
+++ src/leetcode/CourseScheduleII210.java
@@ -7,35 +7,36 @@ import java.util.Queue;
 public class CourseScheduleII210 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<numCourses; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
         Queue<Integer> q = new LinkedList<>();
         int[] indegree = new int[numCourses];
-        ArrayList<Integer> ans = new ArrayList<>();
 
-        for(int i=0; i<prerequisites.length; i++) {
+        int cnt = 0;
+        int[] ans = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
             indegree[prerequisites[i][0]]++;
         }
 
-        for(int i=0; i<numCourses; i++) {
-            if(indegree[i] == 0) {
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 q.add(i);
             }
         }
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int front = q.poll();
-            ans.add(front);
+            ans[cnt++] = front;
 
-            for(int n : adj.get(front)) {
+            for (int n : adj.get(front)) {
                 indegree[n]--;
-                if(indegree[n] == 0) {
+                if (indegree[n] == 0) {
                     q.add(n);
                 }
             }
         }
 
-        if(ans.size() != numCourses) return new int[0];
-        return ans.stream().mapToInt(i -> i).toArray();
+        if (cnt != numCourses) return new int[0];
+        return ans;
     }
 }
