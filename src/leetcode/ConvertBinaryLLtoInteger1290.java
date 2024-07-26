097438401b4e48bad17fe01e97f0e64a872ceb4b
rohan <itsrohan999@gmail.com>
2024-07-25 21:56:26 -0400

    Added LeetCode 1290

---
+++ src/leetcode/ConvertBinaryLLtoInteger1290.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class ConvertBinaryLLtoInteger1290 {
+    public int getDecimalValue(ListNode head) {
+        int sum = 0;
+        while(head!=null){
+            sum = sum * 2 + head.val;
+            head = head.next;
+        }
+
+        return sum;
+    }
+}
