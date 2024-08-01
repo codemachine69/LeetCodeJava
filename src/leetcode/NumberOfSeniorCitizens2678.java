655ff9b2b2ba8a98f28ecb0a090b6b189ecbe725
rohan <itsrohan999@gmail.com>
2024-08-01 02:33:21 -0400

    Added LeetCode 2678

---
+++ src/leetcode/NumberOfSeniorCitizens2678.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class NumberOfSeniorCitizens2678 {
+    public int countSeniors(String[] details) {
+        int cnt = 0;
+        for (String s : details) {
+            int age = Integer.parseInt(s.substring(11, 13));
+            if (age > 60) cnt++;
+        }
+        return cnt;
+    }
+}
