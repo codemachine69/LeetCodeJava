c225eec3d0adbc045ec7b2a9ba3c7104052bfb1d
Rohan Ponuganti <itsrohan999@gmail.com>
2024-11-10 23:55:58 -0500

    Added LeetCode 494

---
+++ src/leetcode/TargetSum494.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class TargetSum494 {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int x : nums) sum += x;
+        if (-sum > target || target > sum) return 0;
+        int[] dp = new int[2 * sum + 1];
+        dp[sum] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int[] next = new int[2 * sum + 1];
+            for (int k = 0; k < 2 * sum + 1; k++) {
+                if (dp[k] != 0) {
+                    next[k + nums[i]] += dp[k];
+                    next[k - nums[i]] += dp[k];
+                }
+            }
+            dp = next;
+        }
+        return dp[sum + target];
+    }
+}
