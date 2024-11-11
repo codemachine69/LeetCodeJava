69874e3fa43237963c1766ec9cac6f069197db09
Rohan Ponuganti <itsrohan999@gmail.com>
2024-11-11 14:30:48 -0500

    Added LeetCode 80

---
+++ src/leetcode/RemoveDuplicatesII80.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class RemoveDuplicatesII80 {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        return j;
+    }
+}
