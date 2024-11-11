42cbd1f851842a530c784faf093347bfb33a0848
Rohan Ponuganti <itsrohan999@gmail.com>
2024-11-11 14:56:47 -0500

    Revised LeetCode 121

--- src/leetcode/BuySellStock121.java
+++ src/leetcode/BuySellStock121.java
@@ -3,21 +3,11 @@ package leetcode;
 public class BuySellStock121 {
 
     public int maxProfit(int[] prices) {
-        int maxProfit = 0, profitIfSellToday = 0, leastPrice = Integer.MAX_VALUE;
+        int profit = 0, buy = -1;
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < leastPrice) leastPrice = prices[i];
-            if (prices[i] - leastPrice > maxProfit) maxProfit = prices[i] - leastPrice;
+            if (buy == -1 || prices[i] <= buy) buy = prices[i];
+            else profit = Math.max(profit, prices[i] - buy);
         }
-
-        return maxProfit;
-    }
-
-    public int maxProfit_kadane(int[] prices) {
-        int maxCur = 0, maxSoFar = 0;
-        for (int i = 1; i < prices.length; i++) {
-            maxCur = Math.max(0, maxCur += prices[i] - prices[i - 1]);
-            maxSoFar = Math.max(maxCur, maxSoFar);
-        }
-        return maxSoFar;
+        return profit;
     }
 }
