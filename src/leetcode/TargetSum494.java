fcd079c90a0e7dfdc5ac58b26ad22900e933124e
Rohan Ponuganti <itsrohan999@gmail.com>
2024-11-11 01:24:40 -0500

    Added LeetCode 494

--- src/leetcode/TargetSum494.java
+++ src/leetcode/TargetSum494.java
@@ -6,20 +6,26 @@ public class TargetSum494 {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
         for (int x : nums) sum += x;
-        if (-sum > target || target > sum) return 0;
-        int[] dp = new int[2 * sum + 1];
-        dp[sum] = 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            int[] next = new int[2 * sum + 1];
-            for (int k = 0; k < 2 * sum + 1; k++) {
-                if (dp[k] != 0) {
-                    next[k + nums[i]] += dp[k];
-                    next[k - nums[i]] += dp[k];
-                }
+        if (Math.abs(target) > sum || (sum + target) % 2 != 0) return 0;
+
+        int s1 = (target + sum) / 2;
+        return subsetCount(nums, s1);
+    }
+
+    private int subsetCount(int[] nums, int target) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][target + 1];
+
+        for (int i = 1; i <= target; i++) dp[0][i] = 0;
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] > j) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
             }
-            dp = next;
         }
-        return dp[sum + target];
+        return dp[n][target];
     }
 }
