9a282cee422efa81bfc748d6aa9d41638028aa46
Rohan Ponuganti <itsrohan999@gmail.com>
2025-01-24 00:24:22 -0500

    Updated LeetCode 198

--- src/leetcode/HouseRobber198.java
+++ src/leetcode/HouseRobber198.java
@@ -2,16 +2,17 @@ package leetcode;
 
 public class HouseRobber198 {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int a = 0, b = nums[0], c = 0;
-        if (n == 1) return 0;
+        return solve(nums, 0, nums.length-1);
+    }
 
-        for (int i = 2; i <= n; i++) {
-            c = Math.max(nums[i - 1] + a, b);
+    private int solve(int[] nums, int start, int end) {
+        int a = 0, b = 0;
+
+        for(int i=start; i<=end; i++) {
+            int temp = Math.max(nums[i]+a, b);
             a = b;
-            b = c;
+            b = temp;
         }
-
-        return c;
+        return b;
     }
 }
