9a7da80f422bd8bd30b14fd5f09f97ee2943568b
Rohan Ponuganti <itsrohan999@gmail.com>
2025-01-28 18:15:47 -0500

    Updates LeetCode 40

--- src/leetcode/CombinationSumII40.java
+++ src/leetcode/CombinationSumII40.java
@@ -6,21 +6,25 @@ import java.util.List;
 
 public class CombinationSumII40 {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, target, 0);
-        return list;
+        solve(nums, target, 0, new ArrayList<>(), res);
+        return res;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
-        if (remain < 0) return;
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
-        else {
-            for (int i = start; i < nums.length; i++) {
-                if (i > start && nums[i] == nums[i - 1]) continue; // skip duplicates
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i + 1);
-                tempList.remove(tempList.size() - 1);
+    private void solve(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if (start >= candidates.length) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] <= target) {
+                curr.add(candidates[i]);
+                solve(candidates, target - candidates[i], i + 1, curr, res);
+                curr.removeLast();
             }
         }
     }
