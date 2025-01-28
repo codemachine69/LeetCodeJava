29adbe28eb44acdeb3fa4302835a550b730bed39
Rohan Ponuganti <itsrohan999@gmail.com>
2025-01-28 18:08:46 -0500

    Updates LeetCode 39

--- src/leetcode/CombinationSum39.java
+++ src/leetcode/CombinationSum39.java
@@ -6,21 +6,23 @@ import java.util.List;
 public class CombinationSum39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        solve(0, candidates, target, res, new ArrayList<>());
+        solve(candidates, target, 0, new ArrayList<>(), res);
         return res;
     }
 
-    private void solve(int idx, int[] arr, int target, List<List<Integer>> res, List<Integer> curr) {
-        if (idx == arr.length) {
-            if (target == 0) res.add(new ArrayList<>(curr));
+    private void solve(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
             return;
         }
+        if (start >= candidates.length) return;
 
-        if (arr[idx] <= target) {
-            curr.add(arr[idx]);
-            solve(idx, arr, target - arr[idx], res, curr);
-            curr.removeLast();
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                curr.add(candidates[i]);
+                solve(candidates, target - candidates[i], i, curr, res);
+                curr.removeLast();
+            }
         }
-        solve(idx + 1, arr, target, res, curr);
     }
 }
